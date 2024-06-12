2c24b045c2d1ec90912cd3a7d18781250b15f451
zuveriya-shaik <96616391+zuveriya-shaik@users.noreply.github.com>
2024-06-12 10:39:49 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,5 +3,7 @@ class hello
     public static void main(String []args)
     {
         System.out.println("Hello! This is a Java Program.");
+        
+        System.out.println("Changes are made here");
     }
 };
