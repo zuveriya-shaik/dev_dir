30f58f8bd937cd58e7d2417771abf45a9776d39c
zuveriya-shaik <96616391+zuveriya-shaik@users.noreply.github.com>
2024-06-12 10:41:49 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,8 +2,13 @@ class hello
 {
     public static void main(String []args)
     {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
         System.out.println("Hello! This is a Java Program.");
         
         System.out.println("Changes are made here");
+        
+        System.out.println(sum);
     }
 };
